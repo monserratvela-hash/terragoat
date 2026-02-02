92cba3df60dc173bf431e93e02bfce0291d5e579
monserratvela-hash <monserratvela@gmail.com>
2026-02-02 16:56:23 -0300

    Update storage.tf

--- terraform/azure/storage.tf
+++ terraform/azure/storage.tf
@@ -26,7 +26,6 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
-  enable_https_traffic_only = true
   queue_properties {
     logging {
       delete                = false
